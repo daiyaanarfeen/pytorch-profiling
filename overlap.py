642f85581c9474efebd1914e8724083ad703b382
root <root@phortx2.cluster.local>
2021-10-06 21:24:50 +0000

    distributed training step

--- overlap.py
+++ overlap.py
@@ -1,25 +1,32 @@
 import os
 import argparse
 import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
 import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 
 """ All-Reduce example."""
 def run():
     """ Simple collective communication. """
     rank = dist.get_rank()
-    model = 
+    model = torchvision.models.resnet18() 
     model.to(rank)
     ddp_model = DDP(model, device_ids=[rank])
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
     optimizer.zero_grad()
+    inputs = torch.randn(32, 3, 224, 244).to(rank)
+    labels = torch.randn(32, 1000).to(rank)
 
-    outputs = ddp_model()
+    outputs = ddp_model(inputs)
     loss_fn(outputs, labels).backward()
     optimizer.step()
 
-    print('Rank ', dist.get_rank(), ' has data ', tensor[0])
+    print('success')
+
 
 def init_process(fn, backend='gloo'):
     """ Initialize the distributed environment. """
