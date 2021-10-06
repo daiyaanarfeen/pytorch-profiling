89e0f0aeb195c212127e4aecfd888019b71828e7
root <root@phortx2.cluster.local>
2021-10-06 22:03:15 +0000

    profiler

--- overlap.py
+++ overlap.py
@@ -5,6 +5,7 @@ import torchvision
 import torch.nn as nn
 import torch.optim as optim
 import torch.distributed as dist
+import torch.profiler
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 
@@ -21,9 +22,16 @@ def run():
     inputs = torch.randn(32, 3, 224, 244).to(rank)
     labels = torch.randn(32, 1000).to(rank)
 
-    outputs = ddp_model(inputs)
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
+    with torch.profiler.profile(
+#            schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/resnet18'),
+            record_shapes=True,
+            with_stack=True
+    ) as prof:
+        outputs = ddp_model(inputs)
+        loss_fn(outputs, labels).backward()
+        optimizer.step()
+        prof.step()
 
     print('success')
 
--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.launch --nproc_per_node=2 --nnodes=2 --node_rank=0 --master_addr=phortx2 --master_port=1234 overlap.py
+python -m torch.distributed.launch --nproc_per_node=$1 --nnodes=2 --node_rank=$2 --master_addr=phortx2 --master_port=1234 overlap.py
