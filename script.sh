5e1a4f3da56f3e609dddac691fd95ad03675a84e
root <root@phortx2.cluster.local>
2021-10-07 18:40:43 +0000

    script args

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-python -m torch.distributed.launch --nproc_per_node=$1 --nnodes=2 --node_rank=$2 --master_addr=phortx2 --master_port=1234 overlap.py
+rank=$1
+for i in $(seq $2 $3)
+do
+	numactl --cpunodebind=$(($i < 4 ? 0 : 1)) --membind=$(($i < 4 ? 0 : 1)) python overlap.py --master_addr phortx3 --master_port 5678 --world_size $4 --rank $rank --log_dir $5 & 
+	rank=$((rank+1))
+done
