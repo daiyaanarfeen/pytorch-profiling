1cd0430e801a26878e8bbb7e20c846c02fefa0fc
Daiyaan Arfeen <daiyaanarfeen@cmu.edu>
2021-11-29 20:29:08 +0000

    pollux models

---
+++ pollux_models_dist_prof.py
@@ -0,0 +1,76 @@
+import os
+import argparse
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributed as dist
+import torch.profiler
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.multiprocessing as mp
+
+""" All-Reduce example."""
+def run(log_dir, batch_size, model):
+    """ Simple collective communication. """
+    rank = dist.get_rank()
+    bs = batch_size[0] if len(batch_size) == 1 else batch_size[rank]
+
+    if model == 'resnet50':
+        model = torchvision.models.resnet50()
+        loss_fn = nn.MSELoss()
+        inputs = torch.randn(bs, 3, 224, 224).cuda()
+        labels = torch.randn(bs, 1000).cuda()
+    elif model == 'deepspeech2':
+        pass
+    elif model == 'yolov3':
+        pass
+    elif model == 'ncf':
+        pass
+
+    model.cuda()
+    ddp_model = DDP(model, broadcast_buffers=False, bucket_cap_mb=25)
+    optimizer = optim.AdamW(ddp_model.parameters())
+
+
+    with torch.profiler.profile(
+            schedule=torch.profiler.schedule(wait=0, warmup=5, active=5, repeat=1),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler(log_dir),
+            record_shapes=True,
+            with_stack=True
+    ) as prof:
+        for i in range(10):
+            optimizer.zero_grad()
+            outputs = ddp_model(inputs)
+            loss_fn(outputs, labels).backward()
+            optimizer.step()
+            prof.step()
+
+    print('success')
+
+
+def init_process(fn, log_dir, batch_size, model):
+    """ Initialize the distributed environment. """
+    dist.init_process_group(backend='nccl', init_method='env://')
+    fn(log_dir, batch_size, model)
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--log_dir", type=str)
+    parser.add_argument("--master_addr", type=str)
+    parser.add_argument("--master_port", type=str)
+    parser.add_argument("--world_size", type=str)
+    parser.add_argument("--rank", type=str)
+    parser.add_argument("--device", type=int)
+    parser.add_argument("--batch_size", type=int, nargs="+") # always a list
+    args = parser.parse_args()
+
+    os.environ["MASTER_ADDR"] = args.master_addr
+    os.environ["MASTER_PORT"] = args.master_port
+    os.environ["WORLD_SIZE"] = args.world_size
+    os.environ["RANK"] = args.rank
+
+    torch.cuda.set_device(args.device)
+    init_process(run, args.log_dir, args.batch_size, args.model)
--- script.sh
+++ script.sh
@@ -4,11 +4,13 @@ cuda_dev_start=${args[1]}
 cuda_dev_end=${args[2]}
 world_size=${args[3]}
 log_dir=${args[4]}
-batch_size=${args[@]:5}
+model=${args[5]}
+batch_size=${args[@]:6}
 
 for i in $(seq $cuda_dev_start $cuda_dev_end)
 do
 	numactl --cpunodebind=$(($i < 4 ? 0 : 1)) --membind=$(($i < 4 ? 0 : 1)) \
-		python overlap.py --master_addr phortx2 --master_port 5678 --world_size $world_size --rank $rank --device $i --log_dir $log_dir --batch_size $batch_size &
+		python pollux_models_dist_prof.py --master_addr phodgx2 --master_port 5678 --world_size $world_size --rank $rank --device $i --log_dir $log_dir \
+		--model $model --batch_size $batch_size &
 	rank=$((rank+1))
 done
